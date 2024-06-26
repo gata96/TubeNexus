27391050d3eaa7d1d6bf24b2101603ee39fb61d4
gata96 <121420427+gata96@users.noreply.github.com>
2024-06-26 16:54:55 +0900

    Get request

--- src/server.js
+++ src/server.js
@@ -2,9 +2,21 @@ import express from "express";
 
 const PORT = 4000;
 
-const app = express();
+const app = express(); // 1. application 만들기
 
-const handleListening = () => console.log("Server listening on port http://localhost:${PORT}  🚀");
+const handleHome = (req, res) => { // 2. 서버에서 client로 Respond
+    return res.send("I Love you Forever");  
+}; 
 
-app.listen(PORT, handleListening);
+const handleLogin = (req, res) => {
+    return res.send("Login here.");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () =>
+    console.log(`Server listening on port http://localhost:${PORT}🚀`);
+
+app.listen(PORT, handleListening); // 3. Server는 계속 client의 요구(request)를 'listen'하고 있다.
 
